c3568d4a032195b2e054b6586c39b0806779544c
s.morovati <simin.morovatii@gmail.com>
2024-05-28 11:53:39 +0330

    Session02

---
+++ src/Session02.java
@@ -0,0 +1,18 @@
+public class Session02 {
+    public static void main(String[] args) {
+        String name = "simin";
+        String family = "morovati";
+        int age = 36;
+        boolean married = true;
+        float avg = 17.56f;
+        float height = 156.2f;
+        String gender = "female";
+
+        System.out.println("name: " + name);
+        System.out.println("family: " + family);
+        System.out.println("age: " + age);
+        System.out.println("height: " + height);
+        System.out.println("married: " + married);
+        System.out.println("gender: " + gender);
+    }
+}
